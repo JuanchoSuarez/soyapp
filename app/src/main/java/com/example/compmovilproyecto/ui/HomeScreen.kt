cd13213032fb6dddbb2354220a95f00d652c730d
Eliana Pardo <elianapardo320@gmail.com>
2026-02-11 23:22:35 -0500

    feat: agregar composable para una imagen generica

--- app/src/main/java/com/example/compmovilproyecto/ui/HomeScreen.kt
+++ app/src/main/java/com/example/compmovilproyecto/ui/HomeScreen.kt
@@ -1,14 +1,17 @@
 package com.example.compmovilproyecto.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.compmovilproyecto.R
 
 @Composable
 fun TextoGeneral(
@@ -40,7 +43,29 @@ fun BotonGeneral(
 }
 
 @Composable
-@Preview(showBackground = true)
+@Preview()
 fun BotonGeneralPreview(){
     BotonGeneral("Iniciar Sesión")
+}
+
+@Composable
+fun ImagenGenerica(
+    idImagen: Int, // (R.drawable.nombreImagen)
+    descripcion: String,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(id = idImagen),
+        contentDescription = descripcion,
+        modifier = modifier
+    )
+}
+
+@Composable
+@Preview()
+fun ImagenGenericaPreview() {
+    ImagenGenerica(
+        idImagen = R.drawable.iconosoy,
+        descripcion = "Logo CompMovil"
+    )
 }
