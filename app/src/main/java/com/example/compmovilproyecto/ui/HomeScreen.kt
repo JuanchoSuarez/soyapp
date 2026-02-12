6f3b3bec7121a654a5d0fb16f41e65eaa2067e20
Eliana Pardo <elianapardo320@gmail.com>
2026-02-11 23:52:09 -0500

    feat: agregar BodyHomeScreen

--- app/src/main/java/com/example/compmovilproyecto/ui/HomeScreen.kt
+++ app/src/main/java/com/example/compmovilproyecto/ui/HomeScreen.kt
@@ -1,13 +1,18 @@
 package com.example.compmovilproyecto.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -16,10 +21,14 @@ import com.example.compmovilproyecto.R
 @Composable
 fun TextoGeneral(
     mensaje: String,
-    modifier: Modifier = Modifier // Parametro opcional siguiendo el patron
+    color: Color = Color.White,
+    estilo: TextStyle = TextStyle.Default,
+    modifier: Modifier = Modifier
 ) {
     Text(
         text = mensaje,
+        color = color,
+        style = estilo,
         modifier = modifier
     )
 }
@@ -49,7 +58,7 @@ fun BotonGeneralPreview(){
 }
 
 @Composable
-fun ImagenGenerica(
+fun ImagenGeneral(
     idImagen: Int, // (R.drawable.nombreImagen)
     descripcion: String,
     modifier: Modifier = Modifier
@@ -63,9 +72,39 @@ fun ImagenGenerica(
 
 @Composable
 @Preview()
-fun ImagenGenericaPreview() {
-    ImagenGenerica(
+fun ImagenGeneralPreview() {
+    ImagenGeneral(
         idImagen = R.drawable.iconosoy,
         descripcion = "Logo CompMovil"
     )
 }
+
+@Composable
+fun BodyHomeScreen(){
+    Column{
+        TextoGeneral("SOY",
+            estilo = TextStyle(fontSize = 50.sp,
+                fontFamily = FontFamily.Cursive),
+            modifier = Modifier)
+        ImagenGeneral(
+            idImagen = R.drawable.iconosoy,
+            descripcion = "Logo CompMovil"
+        )
+        TextoGeneral("\"Live a life you will remember\"",
+            estilo = TextStyle(fontSize = 15.sp,
+                fontStyle = FontStyle.Italic)
+            )
+        TextoGeneral("- Avicii, The Nights",
+            estilo = TextStyle(fontSize = 10.sp)
+        )
+        BotonGeneral("Sing Up")
+        BotonGeneral("Log in")
+    }
+}
+
+@Composable
+@Preview()
+fun BodyHomeScreenPreview(){
+    BodyHomeScreen()
+}
+
