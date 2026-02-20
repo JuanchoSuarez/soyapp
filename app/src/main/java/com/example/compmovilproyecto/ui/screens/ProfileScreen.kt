69825e238bc1f5ad86cbd39b5b1598a1d6f46f20
Laura Lara <larala7789Qgmail.com>
2026-02-20 05:07:12 -0500

    Creación de Header para Profile Screen

--- app/src/main/java/com/example/compmovilproyecto/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/compmovilproyecto/ui/screens/ProfileScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,8 +30,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compmovilproyecto.R
-import com.example.compmovilproyecto.ui.data.Review
+import com.example.compmovilproyecto.ui.data.local.LocalReviewProvider
+import com.example.compmovilproyecto.ui.utils.BackgroundPlano
 import com.example.compmovilproyecto.ui.utils.BackgroundPlanoSuperior
+import com.example.compmovilproyecto.ui.utils.FooterExplorerScreen
 import com.example.compmovilproyecto.ui.utils.ReviewCard
 
 @Composable
@@ -61,7 +65,7 @@ fun HeaderProfileScreen(){
                 fontSize = 10.sp,
                 fontWeight = FontWeight.Bold
             )
-            Row(){
+            Row{
                 Text(
                     modifier = Modifier.padding(top = 12.dp),
                     text =" 2",
@@ -87,8 +91,8 @@ fun HeaderProfileScreen(){
                 )
 
             }
-            Column(){
-                Row(){
+            Column{
+                Row{
                     Text(
                         modifier = Modifier.padding(top = 12.dp),
                         text ="Resenas",
@@ -118,7 +122,74 @@ fun HeaderProfileScreen(){
     }
 }
 
+@Composable
+fun ProfileScreen() {
+    Scaffold(
+        bottomBar = {
+            FooterExplorerScreen()
+        }
+    ) { paddingValores ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValores)
+        ) {
+            HeaderProfileScreen()
+            BodyProfileScreen(
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+        }
+    }
+}
+@Composable
+fun BodyProfileScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        BackgroundPlanoSuperior()
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            ReviewList(modifier = Modifier.weight(1f), textoId = R.string.mis_reseñas)
+        }
+    }
+}
+@Preview (showBackground = true)
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen()
+}
 
+@Composable
+fun ReviewList(
+    modifier: Modifier = Modifier,
+    textoId: Int
+) {
+    val reviews = LocalReviewProvider.reviews
+
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+
+        item {
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = stringResource(textoId),
+                color = Color.White,
+                fontSize = 16.sp,
+                modifier = Modifier.padding(bottom = 12.dp),
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        items(reviews) { review ->
+            ReviewCard(review = review)
+            Spacer(modifier = Modifier.height(17.dp))
+        }
+    }
+}
 @Composable
 fun ProfileImage(
     modifier: Modifier = Modifier,
